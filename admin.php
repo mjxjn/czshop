eeac13f47425f025e81e17880cdd2cff74a52fb5
mjxjn <mjxjn@vip.qq.com>
2013-08-08 00:37:45 +0800

    更新后台框架

--- admin.php
+++ admin.php
@@ -3,7 +3,6 @@
 define('BUILD_DIR_SECURE', true); 
 define('DIR_SECURE_FILENAME', 'index.html');
 define('DIR_SECURE_CONTENT', 'deney Access!'); 
-'DEFAULT_TIMEZONE'=>'Asia/Shanghai' // 设置默认时区为新加坡
 
 define('APP_NAME','admin');
 define('APP_PATH','./admin/');
--- admin/Runtime/Cache/109214985e10a53934a79914ccac7bf7.php
+++
@@ -1,5 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?>__JS__
-__PUBLIC__
-__APP__
-__ROOT__
-../Public
